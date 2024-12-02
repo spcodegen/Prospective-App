d13f33fb851f784eef54bda9e071c6adb1494210
spcodegen <shehansp2000@gmail.com>
2024-12-02 16:46:01 +0530

    loginscreen and home screen changes using local storage

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -14,10 +14,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  //form key for the form validation
   final _formKey = GlobalKey<FormState>();
 
-  //controller for the text from feilds
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
@@ -58,71 +56,71 @@ class _LoginScreenState extends State<LoginScreen> {
                     height: 16,
                   ),
                   Form(
-                      key: _formKey,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Column(
-                          children: [
-                            _buildTextField(
-                              controller: _userNameController,
-                              labelText: 'User Name',
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter User Name';
-                                }
-                                return null;
-                              },
-                              isNumeric: true,
-                            ),
-                            const SizedBox(
-                              height: 14,
-                            ),
-                            _buildTextField(
-                              controller: _passwordController,
-                              obscureText: true,
-                              labelText: 'Password',
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter Password';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(
-                              height: 25,
-                            ),
-                            SizedBox(
-                              width: 280,
-                              height: 50,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  backgroundColor: Colors.green,
-                                  elevation: 10,
-                                  shadowColor:
-                                      const Color.fromARGB(255, 6, 6, 6),
+                    key: _formKey,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          _buildTextField(
+                            controller: _userNameController,
+                            labelText: 'User Name',
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter User Name';
+                              }
+                              return null;
+                            },
+                            isNumeric: true,
+                          ),
+                          const SizedBox(
+                            height: 14,
+                          ),
+                          _buildTextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            labelText: 'Password',
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter Password';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          SizedBox(
+                            width: 280,
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                onPressed: () async {
-                                  if (_formKey.currentState!.validate()) {
-                                    await _loginUser();
-                                  }
-                                },
-                                child: const Text(
-                                  'Login Now',
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontFamily: 'Georgia',
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                                backgroundColor: Colors.green,
+                                elevation: 10,
+                                shadowColor: const Color.fromARGB(255, 6, 6, 6),
+                              ),
+                              onPressed: () async {
+                                if (_formKey.currentState!.validate()) {
+                                  await _loginUser();
+                                }
+                              },
+                              child: const Text(
+                                'Login Now',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontFamily: 'Georgia',
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                      ))
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -146,21 +144,48 @@ class _LoginScreenState extends State<LoginScreen> {
         // Decode the JSON response
         final Map<String, dynamic> jsonData = json.decode(response.body);
 
-        //get data json object using
+        // Extract all the values from the JSON object
+        final int id = jsonData['id'];
+        final String bucode = jsonData['bucode'];
+        final String percode = jsonData['percode'];
         final String name = jsonData['name'];
+        final String soflevelcode = jsonData['soflevelcode'];
+        final String sotdesc = jsonData['sotdesc'];
         final String branch = jsonData['branch'];
-        //save the userName,name,branch in the device storage
+        final String contactno = jsonData['contactno'];
+        final String overrider = jsonData['overrider'];
+        final String address = jsonData['address'];
+        final String brId = jsonData['brId'];
+        final String regonid = jsonData['regonid'];
+        final String zoneid = jsonData['zoneid'];
+        final String? pw = jsonData['pw']; // Can be null
+        final String? status = jsonData['status']; // Can be null
+
+        // Save all the user details
         await UserServices.storeUserDetails(
+          id: id,
+          bucode: bucode,
           userName: userName,
+          percode: percode,
           name: name,
+          soflevelcode: soflevelcode,
+          sotdesc: sotdesc,
           branch: branch,
+          contactno: contactno,
+          overrider: overrider,
+          address: address,
+          brId: brId,
+          regonid: regonid,
+          zoneid: zoneid,
+          pw: pw,
+          status: status,
           context: context,
         );
 
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => MainScreen(), // Pass username
+            builder: (context) => MainScreen(),
           ),
         );
       } else {
--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -2,65 +2,135 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserServices {
-  //method to store the userName in shared pref
+  // Method to store user details in shared preferences
   static Future<void> storeUserDetails({
+    required int id,
     required String userName,
+    required String bucode,
+    required String percode,
     required String name,
+    required String soflevelcode,
+    required String sotdesc,
     required String branch,
+    required String contactno,
+    required String overrider,
+    required String address,
+    required String brId,
+    required String regonid,
+    required String zoneid,
+    required String? pw, // Can be null
+    required String? status, // Can be null
     required BuildContext context,
   }) async {
     try {
-      //create an instance from shared pref
+      // Create an instance of SharedPreferences
       SharedPreferences prefs = await SharedPreferences.getInstance();
-      //store the user name and email as key value pairs
-      await prefs.setString("username", userName);
-      await prefs.setString("name", name);
-      await prefs.setString("branch", branch);
-      print('User data saved: username=$userName, name=$name, branch=$branch');
 
-      //show a massage to the user
+      // Store all values as key-value pairs
+      await prefs.setInt("id", id);
+      await prefs.setString("bucode", bucode);
+      await prefs.setString("username", userName);
+      await prefs.setString("percode", percode);
+      await prefs.setString("name", name);
+      await prefs.setString("soflevelcode", soflevelcode);
+      await prefs.setString("sotdesc", sotdesc);
+      await prefs.setString("branch", branch);
+      await prefs.setString("contactno", contactno);
+      await prefs.setString("overrider", overrider);
+      await prefs.setString("address", address);
+      await prefs.setString("brId", brId);
+      await prefs.setString("regonid", regonid);
+      await prefs.setString("zoneid", zoneid);
+      if (pw != null) {
+        await prefs.setString("pw", pw);
+      }
+      if (status != null) {
+        await prefs.setString("status", status);
+      }
+
+      print(
+          'User data saved: id=$id, name=$name, branch=$branch, username=$userName, bucode=$bucode, percode=$percode, soflevelcode=$soflevelcode, sotdesc=$sotdesc, contactno=$contactno, overrider=$overrider, address=$address, brId=$brId, regonid=$regonid, zoneid=$zoneid, pw=$pw, status=$status');
+
+      // Show a message to the user
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text("User Details stored successfully"),
         ),
       );
     } catch (error) {
-      error.toString();
+      print("Error storing user data: $error");
     }
   }
 
-  //methode to check weather the username is saved in the shared pref
+  // Method to check whether the username is saved in shared preferences
   static Future<bool> checkUsername() async {
-    //create an instance for shared pref
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? userName = prefs.getString('username');
     return userName != null;
   }
 
-  //get the username,name,branch
-  static Future<Map<String, String>> getUserData() async {
-    //create an instance for shared pref
-    SharedPreferences pref = await SharedPreferences.getInstance();
+  // Method to get all user data from shared preferences
+  static Future<Map<String, dynamic>> getUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    String? userName = pref.getString('username');
-    String? name = pref.getString('name');
-    String? branch = pref.getString('branch');
+    int? id = prefs.getInt('id');
+    String? userName = prefs.getString('username');
+    String? bucode = prefs.getString('bucode');
+    String? percode = prefs.getString('percode');
+    String? name = prefs.getString('name');
+    String? soflevelcode = prefs.getString('soflevelcode');
+    String? sotdesc = prefs.getString('sotdesc');
+    String? branch = prefs.getString('branch');
+    String? contactno = prefs.getString('contactno');
+    String? overrider = prefs.getString('overrider');
+    String? address = prefs.getString('address');
+    String? brId = prefs.getString('brId');
+    String? regonid = prefs.getString('regonid');
+    String? zoneid = prefs.getString('zoneid');
+    String? pw = prefs.getString('pw');
+    String? status = prefs.getString('status');
 
     print(
-        "Retrieved from SharedPreferences: username=$userName, name=$name, branch=$branch");
+        "Retrieved from SharedPreferences: id=$id, name=$name, branch=$branch, username=$userName, bucode=$bucode, percode=$percode, soflevelcode=$soflevelcode, sotdesc=$sotdesc, contactno=$contactno, overrider=$overrider, address=$address, brId=$brId, regonid=$regonid, zoneid=$zoneid, pw=$pw, status=$status");
 
     return {
+      'id': id ?? 0,
       'username': userName ?? '',
+      'bucode': bucode ?? '',
+      'percode': percode ?? '',
       'name': name ?? '',
+      'soflevelcode': soflevelcode ?? '',
+      'sotdesc': sotdesc ?? '',
       'branch': branch ?? '',
+      'contactno': contactno ?? '',
+      'overrider': overrider ?? '',
+      'address': address ?? '',
+      'brId': brId ?? '',
+      'regonid': regonid ?? '',
+      'zoneid': zoneid ?? '',
+      'pw': pw ?? '',
+      'status': status ?? '',
     };
   }
 
-  //***remove the username,name,branch from shared preferences
+  // Method to clear user data from shared preferences
   static Future<void> clearUserData() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    await pref.remove('username');
-    await pref.remove('name');
-    await pref.remove('branch');
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('id');
+    await prefs.remove('username');
+    await prefs.remove('bucode');
+    await prefs.remove('percode');
+    await prefs.remove('name');
+    await prefs.remove('soflevelcode');
+    await prefs.remove('sotdesc');
+    await prefs.remove('branch');
+    await prefs.remove('contactno');
+    await prefs.remove('overrider');
+    await prefs.remove('address');
+    await prefs.remove('brId');
+    await prefs.remove('regonid');
+    await prefs.remove('zoneid');
+    await prefs.remove('pw');
+    await prefs.remove('status');
   }
 }
